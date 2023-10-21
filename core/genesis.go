ceefe595b0229039ef111de314d83c02c5ce8462
cyyber <kaushal.forex@gmail.com>
2023-10-22 02:32:21 +0530

    Updated betanet genesis config

--- core/genesis.go
+++ core/genesis.go
@@ -575,7 +575,7 @@ func DefaultBetaNetGenesisBlock() *Genesis {
 		ExtraData:  []byte("BetaNet, Zond, XMSS, Dilithium!!"),
 		GasLimit:   0x1c9c380,
 		Difficulty: big.NewInt(0x1),
-		Timestamp:  1697698958,
+		Timestamp:  1697921722,
 		Alloc:      decodePreallocWithContractCode(betaNetAllocData),
 	}
 }
